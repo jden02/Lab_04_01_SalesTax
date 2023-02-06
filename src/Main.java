2ab7839ed988deae8d2e91addff37139505ea409
jden02 <jden71002@gmail.com>
2023-02-05 19:43:40 -0500

    Task 1 completed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        double price = 10.00;
+        double salesTax;
+        double tax = 0.05;
 
-        
+        salesTax = price * tax;
+
+        System.out.println("Your purchase of $" + price + " has an added tax of $" + salesTax + " for a total of $" + (salesTax+price));
+
     }
 }
